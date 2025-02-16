d5a07ca6e707f3afa4d47e01558a2e2d546c9875
Deepak kumar Rauta <kdeepak8250@gmail.com>
2025-02-16 22:37:01 +0530

    Two Sum Problem

---
+++ Two_sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twosum(self, numbers, target):
+        # Take two pointers
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left+1, right+1]
+            elif total < left:
+                left += 1
+            else:
+                right -= 1
+numbers = [2, 7, 11, 15]
+target = 9
+obj = Solution()
+print(obj.twosum(numbers, target))
