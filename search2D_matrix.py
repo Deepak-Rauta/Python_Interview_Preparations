4a50f9508fb124ca43da8b8ff8d2bbc3a4ac7534
Deepak <kdeepak8250@gmail.com>
2025-03-05 08:41:24 +0530

    Updated 2D matrix

--- search2D_matrix.py
+++ search2D_matrix.py
@@ -1,5 +1,32 @@
+# class Solution:
+#     def Matrix_Solution(self, matrix, target):
+#         m, n = len(matrix), len(matrix[0])
+#         left, right = 0, (m * n)-1
+#         while left <= right:
+#             mid = (left + right)//2
+#             row, col = divmod(mid, n)
+#             mid_value = matrix[row][col]
+#             if mid_value == target:
+#                 return True
+#             elif mid_value < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#         return False
+# matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+# target = 3
+# solution_instance = Solution()
+# print(solution_instance.Matrix_Solution(matrix, target))
+
+
+
+
+
+
 class Solution:
-    def Matrix_Solution(self, matrix, target):
+    def Find_Matrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
         m, n = len(matrix), len(matrix[0])
         left, right = 0, (m * n)-1
         while left <= right:
@@ -16,4 +43,4 @@ class Solution:
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 target = 3
 solution_instance = Solution()
-print(solution_instance.Matrix_Solution(matrix, target))
+print(solution_instance.Find_Matrix(matrix, target))
