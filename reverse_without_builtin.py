be8e4e992cf76a957c92cbf7860350a76ef534ed
Deepak <kdeepak8250@gmail.com>
2025-04-03 18:21:28 +0530

    Reverse a String without using builtin Function

---
+++ reverse_without_builtin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def Reverse(self, s):
+        reversed_str = ""
+        for char in s:
+            reversed_str = char + reversed_str
+        return reversed_str
+string = "Deepak"
+obj = Solution()
+print(obj.Reverse(string))
