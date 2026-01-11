d14223a704ac74a5cc8bc5c001bebf3fe89553fd
Deepak-Rauta <kdeepak8250@gmail.com>
2026-01-11 20:14:19 +0530

    Map & Filter interview based questions

--- map_filter.py
+++ map_filter.py
@@ -52,3 +52,42 @@ Return the final list. Hint: You can put filter() inside map()."""
 data = [1, 2, 3, 4, 5, 6]
 result = list(map(lambda x: x **2, filter(lambda x: x % 2 == 0, data)))
 print(result)
+
+
+# 🎤 The Mock Interview
+
+"""Interview Question:
+
+"I have a list of dictionaries representing transactions. Some transactions are missing the 'amount' key (dirty data).
+
+Write a function called clean_and_sum that:
+
+Accepts a list of transaction dictionaries.
+
+Filters out any transaction that is missing the 'amount' key.
+
+Maps the remaining transactions to extract just the amount values.
+
+Returns the Sum of those amounts.
+
+Constraint: Try to use map, filter, and lambda where appropriate."""
+
+def clean_and_sum(transactions):
+    # Step 1: Filter transactions that have 'amount'
+    valid_transactions = filter(lambda t: 'amount' in t, transactions)
+
+    # step 2: Extract the amoun values
+    amounts = map(lambda t: t['amount'], valid_transactions)
+
+    # step 3: sum the amounts
+    return sum(amounts)
+
+transactions = [
+    {"id": 1, "amount": 100},
+    {"id": 2},                # Missing amount!
+    {"id": 3, "amount": 50}
+]
+
+print(clean_and_sum(transactions))
+
+
