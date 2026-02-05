ba1d9af35adda5d01750ca09b514fd39a4cf2d87
Deepak-Rauta <kdeepak8250@gmail.com>
2026-02-05 23:26:55 +0530

    Plus One Problem with manul Carry

--- PlusOne.py
+++ PlusOne.py
@@ -1,8 +1,29 @@
+# class Solution:
+#     def Plusone(self, digits):
+#         num = int("".join(map(str, digits)))
+#         num += 1
+#         return [int(digit) for digit in str(num)]
+# Digit = [1, 2, 3]
+# obj = Solution()
+# print(obj.Plusone(Digit))
+
+
+
+# Plus one problem with manual carry methods
+
 class Solution:
-    def Plusone(self, digits):
-        num = int("".join(map(str, digits)))
-        num += 1
-        return [int(digit) for digit in str(num)]
-Digit = [1, 2, 3]
+    def PlusOne(self, digits):
+        # Find the length
+        n = len(digits)
+        # Iterate backwords from the last index
+        for i in range(n-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            # If digit is 9 than put 0 and carry 1
+            digits[i] = 0
+        return [1] + digits
+
+digits = [9, 9, 9]
 obj = Solution()
-print(obj.Plusone(Digit))
+print(obj.PlusOne(digits))
