0d23269602096f12828074696a95d7046b615c09
Deepak-Rauta <kdeepak8250@gmail.com>
2026-05-07 21:59:32 +0530

    Adding another solution for majority element

--- majority_elements.py
+++ majority_elements.py
@@ -1,13 +1,36 @@
 class Solution:
-    def FindMajority(self, nums):
+    def Findmajority(self, nums):
+        # Take a empty dictionary
         count_dict = {}
-        n = len(nums)//2 # Majority conditions
-        for num in  nums:
-            count_dict[num] = count_dict.get(num, 0)+1
+        # Majority condition 
+        n = len(nums) // 2 # Based on the example majority element must occer > that number 
+        for num in nums:
+            count_dict[num] = count_dict.get(0, num) + 1
             if count_dict[num] > n:
                 return num
-# Example usages
-numbers = [2,2,1,1,1,2,2]
-solution_instance = Solution()
-result = solution_instance.FindMajority(numbers)
+numbers = [3, 2, 3]
+obj = Solution()
+result = obj.Findmajority(numbers)
 print(result)
+
+
+# Solution:-2
+class Solution:
+    def findMajority(self, nums):
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
+
+numbers = [2, 2, 1, 1, 1, 2, 2]
+obj = Solution()
+print(obj.findMajority(numbers))
