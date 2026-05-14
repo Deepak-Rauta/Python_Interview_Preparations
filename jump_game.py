2d6977eb0323df6ff90888ddd235470ddf8f51bb
Deepak-Rauta <kdeepak8250@gmail.com>
2026-05-14 21:47:10 +0530

    Adding another solution for Jump Game!

--- jump_game.py
+++ jump_game.py
@@ -1,18 +1,41 @@
-# jump game
-# The goal of this code is to determine whether we can reach the last index of the given array nums, 
-# starting from index 0, where each element in nums represents 
-# the maximum number of steps we can jump forward from that position.
+"""You are given an integer array nums. You are initially positioned at the array's first index, 
+and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise."""
 
+# Method:-01
 class Solution:
     def JumpGame(self, nums):
         max_reach = 0
-        for i, jump in enumerate(nums):
+        # Find the last index
+        last_index = len(nums) - 1
+        for i in range(len(nums)):
             if i > max_reach:
                 return False
-            max_reach = max(max_reach, i+jump)
-            if max_reach >= len(nums) - 1:
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= last_index:
                 return True
         return False
-numbers = [2,3,1,1,4]
-solution_instance = Solution()
-print(solution_instance.JumpGame(numbers))
+
+numbers = [2, 3, 1, 1, 4]
+obj = Solution()
+print(obj.JumpGame(numbers))
+
+
+# Jump Game:-
+# Each value in the array tells us from this postion how many maximum steps can i jump forward!
+# Method:-02
+class Solution:
+    def canJump(self, nums):
+        # Our goal is can i continue moving forward without getting stuck!
+        # Initialize the max_reach
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+
+        return True
+
+nums = [2, 3, 1, 1, 4]
+obj = Solution()
+print(obj.canJump(nums))
