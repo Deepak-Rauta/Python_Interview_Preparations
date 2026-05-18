b5add411ea6027665e0db5e76d6768a1a8ebb8db
Deepak-Rauta <kdeepak8250@gmail.com>
2026-05-18 13:38:32 +0530

    Maximum Depth Of Binary Tree

--- max_depth.py
+++ max_depth.py
@@ -4,18 +4,35 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def maxDepth(self, root):
+        # Base case
         if not root:
             return 0
+        
+        # Find the left subtree depth
         left_depth = self.maxDepth(root.left)
-        right_depth = self.maxDepth(root.right)
-        return 1 + max(left_depth, right_depth)
 
-# Example usages
+        # Find the right subtree depth
+        right_depth = self.maxDepth(root.right)
+
+        # Return current depth
+        return 1 + max(left_depth, right_depth)
+    
+# Example Usages
 root = TreeNode(3)
 root.left = TreeNode(9)
 root.right = TreeNode(20, TreeNode(15), TreeNode(7))
 
 solution_instance = Solution()
 print(solution_instance.maxDepth(root))
+
+
+# Important points to remember:-
+# 01. def maxDepth(node) means what is the depth starting from this node
+# 02. For every node the formula is:- depth = 1 + max(left_depth, right_depth)
+# 03. Each node ask its childeren what is your depth? then choose the bigger deoth and add itself.
+# The Time and Space Complexity
+# Time Complexity is:- O(n) Bcz each node is visited once
+# SpaCE cOMPLEXITY IS:- o(h) where h = height of the tree (recurssion stack)    
