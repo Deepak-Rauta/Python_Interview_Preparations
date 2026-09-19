b46756f3e828889ab1dbc05f2eb8e0f6feed723b
Deepak-Rauta <kdeepak8250@gmail.com>
2026-09-19 10:23:25 +0530

    Linked List Basic

---
+++ List_end-to-end.py
@@ -0,0 +1,36 @@
+# Definition:-
+# A list is an ordered, mutable (changeable), and iterable collection of elements.
+# It can store heterogeneous(Different types) data types (integers, strings, floats, objects, etc.).
+
+# Properties:
+
+# Ordered (elements have a fixed order)
+
+# Mutable (you can change elements)
+
+# Duplicates allowed
+
+# Can store mixed data types
+
+# Examples
+my_list = [1, "hello", 3.14, True]
+print(my_list)
+
+# 2️⃣ Creating Lists
+# Ways to create lists in Python:
+# Empty list
+a = []
+
+# with elements
+b = [1, 2, 3]
+
+# using list() constructor
+c = list((4, 5, 6))
+print(c)
+# List is a constructor or functuon in python 
+# it's usede to create a new list in python 
+
+# from a string or range
+d = list("AI")
+e = list(range(5))
+print(e)
